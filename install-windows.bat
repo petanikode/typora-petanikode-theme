ccd48c5a412fe7083bbc60556679d5f530c8ac19
Dian <dian@petanikode.com>
2020-08-15 22:02:44 +0800

    fix: copy command

--- install-windows.bat
+++ install-windows.bat
@@ -1,2 +1,2 @@
-copy petanikode %USERPROFILE%\Roaming\Typora\themes
-copy petanikode.css %USERPROFILE%\Roaming\Typora\themes
+xcopy petanikode %USERPROFILE%\Roaming\Typora\themes /E
+copy petanikode.css %USERPROFILE%\Roaming\Typora\themes /Y
